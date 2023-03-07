2e763a4ebcf24555f94e165e0d18fe46d15a5896
Josue Anes-Paz <JosueRAnes@proton.me>
2023-03-07 15:03:26 -0500

    Add Loader
    Added loader to project.

--- script.js
+++ script.js
@@ -3,16 +3,31 @@ const quoteText = document.querySelector('#quote');
 const authorText = document.querySelector('#author');
 const twitterBtn = document.querySelector('#twitter');
 const newQuoteBtn = document.querySelector('#newQuote');
+const loader = document.querySelector('#loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
 // Show Quote 
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank and replace with 'unknown'
     if (!quote.author) {
-        authorText.textContent = 'Unknown'
+        authorText.textContent = 'Unknown';
     } else {
         authorText.textContent = quote.author;
     } 
@@ -23,11 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiURL);
@@ -46,6 +64,7 @@ function tweetQuote() {
 
 // Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
